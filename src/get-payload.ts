d9eeeaafc199fb6b22177aee2bd6f7a06fcda2eb
YOGI MEIZAL PUTRA <yogimeizalputra@gmail.com>
2024-01-23 02:51:33 +0700

    Setting Up Admin Dashboard

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -9,37 +9,37 @@ dotenv.config({
 
 let cached = (global as any).payload
 
-if(!cached) {
+if (!cached) {
     cached = (global as any).payload = {
-        client : null,
-        promise : null
+        client: null,
+        promise: null
     }
 }
 
 interface Args {
-    initOptions? : Partial<InitOptions>
+    initOptions?: Partial<InitOptions>
 }
 
-export const getPayloadClient = async ({initOptions} : Args = {}) => {
-    if(!process.env.PAYLOAD_SECRET) {
-        throw new Error("PAYLOAD_SECRET is missing")
+export const getPayloadClient = async ({initOptions,}: Args = {}) => {
+    if (!process.env.PAYLOAD_SECRET) {
+        throw new Error('PAYLOAD_SECRET is missing')
     }
 
-    if(cached.client) {
+    if (cached.client) {
         return cached.client
     }
 
-    if(cached.promise) {
+    if (!cached.promise) {
         cached.promise = payload.init({
             secret: process.env.PAYLOAD_SECRET,
             local: initOptions?.express ? false : true,
-            ...(initOptions || {})
+            ...(initOptions || {}),
         })
     }
 
     try {
         cached.client = await cached.promise
-    } catch(e: unknown) {
+    } catch (e: unknown) {
         cached.promise = null
         throw e
     }
--- src/server.ts
+++ src/server.ts
@@ -10,9 +10,9 @@ const start = async () => {
         initOptions: {
             express: app,
             onInit: async (cms) => {
-                cms.logger.info(`Admin URL ${cms.getAdminURL()}`)
-            }
-        }
+                cms.logger.info(`Admin URL: ${cms.getAdminURL()}`)
+            },
+        },
     })
 
     app.use((req, res) => nextHandler(req, res))
@@ -22,7 +22,7 @@ const start = async () => {
 
         app.listen(PORT, async () => {
             payload.logger.info(
-            `Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`
+                `Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`
             )
         })
     })
